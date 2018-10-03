a0d803cea376437436f1074af500ebde14b774db
Con-MI <aspris.marios@gmail.com>
2018-10-03 15:18:15 +0200

    Set indentations to space 4

--- Assignment_2/newton.c
+++ Assignment_2/newton.c
@@ -14,9 +14,9 @@ unsigned int **iterations;
 
 inline double complex newton_step(double complex x_last)
 {
-  double complex ddx = cpow(x_last, d-1); //x^(n-1)
-  // cpow(a,b) is implemented as exp(log(a)*b), maybe it will be faster to implement our own pow function
-  return x_last - (x_last*ddx -1) / (d * ddx);
+     double complex ddx = cpow(x_last, d-1); //x^(n-1)
+    // cpow(a,b) is implemented as exp(log(a)*b), maybe it will be faster to implement our own pow function
+    return x_last - (x_last*ddx -1) / (d * ddx);
 }
 
 //checks whether a solution matches one of the precalculated roots;
@@ -25,128 +25,130 @@ inline double complex newton_step(double complex x_last)
 //returns -1 otherwise
 inline signed char check_solution(double complex solution)
 {
-  for (int i = 0; i < d+1; i++)
-    if (cabs(solution - roots[i]) < LOWER_THRESHOLD)
-      return i;
-  if (abs(creal(solution))>UPPER_THRESHOLD || abs(cimag(solution))>UPPER_THRESHOLD)
-    return d;
-  return -1;
+    for (int i = 0; i < d+1; i++)
+        if (cabs(solution - roots[i]) < LOWER_THRESHOLD)
+        return i;
+    if (abs(creal(solution))>UPPER_THRESHOLD || abs(cimag(solution))>UPPER_THRESHOLD)
+        return d;
+    return -1;
 }
 
 inline signed char compute_point(double complex x) {}
 
 inline void compute_line(int line) {
-  complex double point;
-  int iteration_count;
-  signed char attractor;
-  for (int i = 0; i < nrc; i++)
-  {
-    point = -2 + 4.0*line/nrc  + I * (2 - 4.0*i/nrc);
-    iteration_count = 0;
-    attractor = check_solution(point);
-    while (-1 == attractor) {
-      point = newton_step(point);
-      attractor = check_solution(point);
-      iteration_count++;
+    complex double point;
+    int iteration_count;
+    signed char attractor;
+    for (int i = 0; i < nrc; i++)
+    {
+        point = -2 + 4.0*line/nrc  + I * (2 - 4.0*i/nrc);
+        iteration_count = 0;
+        attractor = check_solution(point);
+        while (-1 == attractor)
+        {
+        point = newton_step(point);
+        attractor = check_solution(point);
+        iteration_count++;
+        }
+        attractors[line][i] = attractor;
+        iterations[line][i] = iteration_count;
     }
-    attractors[line][i] = attractor;
-    iterations[line][i] = iteration_count;
-  }
 }
 
-void *thread_function(void *args)
-{
-  int thread_id = ((int*)args)[0];
-  int linen = thread_id;
-  while (linen < nrc) {
-    compute_line(linen);
-    // TODO here somehow the file writing thread should be informed that there is a line ready to be printed
-    linen += nthreads;
-  }
+void *thread_function(void *args) {
+    int thread_id = ((int*)args)[0];
+    int linen = thread_id;
+    while (linen < nrc)
+    {
+        compute_line(linen);
+        // TODO here somehow the file writing thread should be informed that there is a line ready to be printed
+        linen += nthreads;
+    }
   return NULL;
 }
 
 int main(int argc, char** argv) {
   
-  //get the arguments
-  if (argc < 4)
-    exit(1);
+    //get the arguments
+    if (argc < 4)
+        exit(1);
   
-  nthreads = atoi(argv[1]+2); //number of threads
-  nrc = atoi(argv[2]+2); //number of rows and columns
+    nthreads = atoi(argv[1]+2); //number of threads
+    nrc = atoi(argv[2]+2); //number of rows and columns
 
-  if (d>=10)
-    exit(1);
-  d = atoi(argv[3]); //f(x) = x^d-1
+    if (d>=10)
+        exit(1);
+    d = atoi(argv[3]); //f(x) = x^d-1
 
-  roots = malloc((d+1) * sizeof(*roots));
+    roots = malloc((d+1) * sizeof(*roots));
 
-  // compute the actual results for the roots
-  for (int i = 0; i < d; i++)
-    roots[i] = cos(2* M_PI * i / d) + I * sin(2* M_PI * i / d);
+    // compute the actual results for the roots
+    for (int i = 0; i < d; i++)
+        roots[i] = cos(2* M_PI * i / d) + I * sin(2* M_PI * i / d);
 
-  roots[d] = 0; //workaround to break if the newton algorithm is to close to the origin
-  attractors = malloc(nrc*sizeof(*attractors));
-  iterations = malloc(nrc*sizeof(*iterations));
+    roots[d] = 0; //workaround to break if the newton algorithm is to close to the origin
+    attractors = malloc(nrc*sizeof(*attractors));
+    iterations = malloc(nrc*sizeof(*iterations));
 
-  for (int i = 0; i < nrc; i++)
-  {
-    attractors[i] = malloc(nrc*sizeof(**attractors));
-    iterations[i] = malloc(nrc*sizeof(**iterations));
-  }
-
-  pthread_t *threads;
-  threads = malloc(nthreads * sizeof(*threads));
-  int *thread_args = malloc(nthreads * sizeof(*thread_args));
-
-  for (int tx = 0; tx<nthreads; tx++)
-  {
-    //start the threads
-    thread_args[tx] = tx;
-    int ret;
-    if(ret = pthread_create(threads+tx, NULL, thread_function, (void*)(thread_args+tx))) {
-      printf("Error creating thread: %d, error code %d\n", tx, ret);
-      exit(1);
-    }
-  }
-
-  //finish the threads
-  for (int tx=0; tx < nthreads; ++tx) {
-    int ret;
-    if (ret = pthread_join(threads[tx], NULL))
+    for (int i = 0; i < nrc; i++)
     {
-      printf("Error joining thread: %d\n", ret);
-      exit(1);
+        attractors[i] = malloc(nrc*sizeof(**attractors));
+        iterations[i] = malloc(nrc*sizeof(**iterations));
     }
-  }
+
+    pthread_t *threads;
+    threads = malloc(nthreads * sizeof(*threads));
+    int *thread_args = malloc(nthreads * sizeof(*thread_args));
+
+    for (int tx = 0; tx<nthreads; tx++)
+    {
+        //start the threads
+        thread_args[tx] = tx;
+        int ret;
+        if(ret = pthread_create(threads+tx, NULL, thread_function, (void*)(thread_args+tx))) 
+        {
+            printf("Error creating thread: %d, error code %d\n", tx, ret);
+            exit(1);
+        }
+    }
+
+    //finish the threads
+    for (int tx=0; tx < nthreads; ++tx) {
+        int ret;
+        if (ret = pthread_join(threads[tx], NULL))
+        {
+            printf("Error joining thread: %d\n", ret);
+            exit(1);
+        }
+    }
   
-  //write to file
-  char name_attr[] = "newton_attractors_xd.ppm";
-  char name_conv[] = "newton_convergence_xd.ppm";
-  name_attr[19] = d+'0';
-  name_conv[20] = d+'0';
-  FILE *fa = fopen(name_attr, "w");
-  FILE *fc = fopen(name_conv, "w");
+    //write to file
+    char name_attr[] = "newton_attractors_xd.ppm";
+    char name_conv[] = "newton_convergence_xd.ppm";
+    name_attr[19] = d+'0';
+    name_conv[20] = d+'0';
+    FILE *fa = fopen(name_attr, "w");
+    FILE *fc = fopen(name_conv, "w");
 
-  fprintf(fa, "P3\n%d %d\n%d\n", nrc, nrc, d);
-  fprintf(fc, "P3\n%d %d\n%d\n", nrc, nrc, d);
+    fprintf(fa, "P3\n%d %d\n%d\n", nrc, nrc, d);
+    fprintf(fc, "P3\n%d %d\n%d\n", nrc, nrc, d);
 
-  //TODO Here apparently a maximum number of iterations should be known. I dont know how we can obtain it though.
-  for (int i = 0; i< nrc; ++i)
-  {
-    for (int j = 0; j < nrc; ++j)
-      fprintf(fa, "%d ", attractors[i][j]);
-    fprintf(fa, "\n");
-    for (int j = 0; j < nrc; ++j)
-      fprintf(fc, "%d ", iterations[i][j]);
-    fprintf(fc, "\n");
-  }
+    //TODO Here apparently a maximum number of iterations should be known. I dont know how we can obtain it though.
+    for (int i = 0; i< nrc; ++i)
+    {
+        for (int j = 0; j < nrc; ++j)
+            fprintf(fa, "%d ", attractors[i][j]);
+        fprintf(fa, "\n");
+        for (int j = 0; j < nrc; ++j)
+            fprintf(fc, "%d ", iterations[i][j]);
+        fprintf(fc, "\n");
+    }
 
-  free(roots);
-  free(threads);
-  free(thread_args);
-  free(attractors);
-  free(iterations);
+    free(roots);
+    free(threads);
+    free(thread_args);
+    free(attractors);
+    free(iterations);
 
-  return 0;
+    return 0;
 }
