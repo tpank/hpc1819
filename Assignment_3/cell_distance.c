877a15219d853edef2c9a087adaaa11015f285b9
Tobias Pankert <tobias.pankert@rwth-aachen.de>
2018-10-26 21:07:42 +0200

    implement blockwise file reading for big input files

--- Assignment_3/cell_distance.c
+++ Assignment_3/cell_distance.c
@@ -11,7 +11,7 @@
 #define MAX(a,b) (a>b ? a : b)
 #define MIN(a,b) (a<b ? a : b)
 #define NDISTANCES 3465 //round(100*sqrt(20^2+20^2+20^2)+1
-#define MAX_BLOCK_SIZE ((unsigned long)1<<27)
+#define MAX_BLOCK_SIZE ((unsigned long)1<<20)
 typedef struct coordinates {
   short x, y, z; //these are the coordinates * 1000
 } coordinates_t;
@@ -43,12 +43,24 @@ short read_coordinate(char *restrict str)
 }
 
 inline
-void read_coordinates(coordinates_t * input, char *restrict str)
+void read_coordinates(coordinates_t *restrict input, char *restrict str)
 {
   input->x = read_coordinate(str);
   input->y = read_coordinate(str+8);
   input->z = read_coordinate(str+16);
 }
+
+inline
+void read_block(size_t offset, size_t block_size, FILE *file, char *buffer, coordinates_t *input)
+{
+  fseek(file, 24 * offset, SEEK_SET);
+  fread(buffer, 1, 24 * block_size, file);
+
+#pragma omp parallel for
+  for (size_t m = 0; m < block_size; m++)
+    read_coordinates(input+m, buffer+24*m);
+}
+
 int main(int argc, char *argv[])
 {
   if (argc < 2 || !strcmp(argv[1], "-t")) {
@@ -76,42 +88,34 @@ int main(int argc, char *argv[])
   coordinates_t * const input_j_backup = input_j;
   char *buffer = malloc(block_size * 24);
 
+  for (size_t ib = 0; ib < ncoordinates; ib+=block_size) {
+    size_t i_block_size = ib + block_size < ncoordinates ? block_size : ncoordinates -ib;
+    read_block(ib, i_block_size, fp, buffer, input_i);
+    for (size_t jb = ib; jb < ncoordinates; jb+=block_size) {
+      size_t j_block_size = jb + block_size < ncoordinates ? block_size : ncoordinates - jb;
+      if (jb != ib)
+	read_block(jb, j_block_size, fp, buffer, input_j);
+      else
+	input_j = input_i;
+
+      
 #pragma omp parallel
-  {
-    const int ithread = omp_get_thread_num();
-
-    for (size_t ib = 0; ib < ncoordinates; ib+=block_size) {
-      size_t ie = ib + block_size < ncoordinates ? block_size : ncoordinates -ib;
-      fseek(fp, ib, SEEK_SET);
-      fread(buffer, 1, 24 * block_size, fp);
-#pragma omp for
-      for (size_t m = 0; m < block_size; m++)
-	read_coordinates(input_i+m, buffer+24*m);
-
-      for (size_t jb = ib; jb < ncoordinates; jb+=block_size) {
-	size_t je = jb + block_size < ncoordinates ? block_size : ncoordinates - jb;
-	if (jb != ib) {
-	  fseek(fp, jb, SEEK_SET);
-	  fread(buffer, 1, 24 * block_size, fp);
-#pragma omp for
-	  for (size_t m = 0; m < block_size; m++)
-	    read_coordinates(input_j+m, buffer+24*m);
-	} else {
-	  input_j = input_i;
-	}
-#pragma omp for schedule(dynamic, 100)
-	for (size_t ix=0; ix < ie; ++ix)
-	  for (size_t jx = (ib < jb ? 0: ix+1); jx < je; ++jx)
+      {
+       	const int ithread = omp_get_thread_num(); 
+#pragma omp for schedule(dynamic, 1000)
+	for (size_t ix = 0; ix < i_block_size; ++ix)
+	  for (size_t jx = (ib < jb ? 0 : ix+1); jx < j_block_size; ++jx)
 	    distance_counters[NDISTANCES*ithread + compute_distance(input_i+ix, input_j+jx)]++;
-	
-	input_j = input_j_backup;
       }
+      input_j = input_j_backup;
     }
-#pragma omp for
-    for (size_t i = 0; i < NDISTANCES; i++)
-      for (size_t j = 1; j < nthreads; j++)
-	distance_counters[i]+=distance_counters[NDISTANCES*j + i];
   }
+  
+  //#pragma omp parallel for
+  for (size_t i = 0; i < NDISTANCES; i++)
+    for (size_t j = 1; j < nthreads; j++)
+      distance_counters[i]+=distance_counters[NDISTANCES*j + i];
+  
   fclose(fp);
   fp = NULL;
   free(input_i);
