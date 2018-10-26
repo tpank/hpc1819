00f3e7b4297d600800516b6ec0d0a8d091a32a0e
Tobias Pankert <tobias.pankert@rwth-aachen.de>
2018-10-26 18:16:33 +0200

    add block-based for loops to avoid the programm becoming too large

--- Assignment_3/cell_distance.c
+++ Assignment_3/cell_distance.c
@@ -7,8 +7,11 @@
 #include <math.h>
 #include <string.h>
 
-#define NDISTANCES 3465 //round(100*sqrt(20^2+20^2+20^2)+1
 
+#define MAX(a,b) (a>b ? a : b)
+#define MIN(a,b) (a<b ? a : b)
+#define NDISTANCES 3465 //round(100*sqrt(20^2+20^2+20^2)+1
+#define MAX_BLOCK_SIZE ((unsigned long)1<<27)
 typedef struct coordinates {
   short x, y, z; //these are the coordinates * 1000
 } coordinates_t;
@@ -26,7 +29,7 @@ unsigned short compute_distance(coordinates_t * restrict a, coordinates_t * rest
 }
 
 inline
-short read_coordinates(char *str)
+short read_coordinate(char *restrict str)
 {
   //str has format +03.123
   short v =
@@ -39,6 +42,13 @@ short read_coordinates(char *str)
   return str[0] == '+' ? v : -v;
 }
 
+inline
+void read_coordinates(coordinates_t * input, char *restrict str)
+{
+  input->x = read_coordinate(str);
+  input->y = read_coordinate(str+8);
+  input->z = read_coordinate(str+16);
+}
 int main(int argc, char *argv[])
 {
   if (argc < 2 || !strcmp(argv[1], "-t")) {
@@ -58,40 +68,57 @@ int main(int argc, char *argv[])
     exit(1);
   }
   size_t ncoordinates = st.st_size / 24; //24 characters/line
-
-  coordinates_t *input = malloc(ncoordinates * sizeof(*input));
-  char *buffer = malloc(25);
-  for (size_t i = 0; i < ncoordinates; i++) {
-    if (NULL == fgets(buffer, 25, fp))
-      exit(1);
-    input[i].x = read_coordinates(buffer);
-    input[i].y = read_coordinates(buffer+8);
-    input[i].z = read_coordinates(buffer+16);  
-  }
-  free(buffer);
-  buffer = NULL;
-  fclose(fp);
-  fp = NULL;
-  
+  const size_t block_size = MIN(MAX_BLOCK_SIZE, ncoordinates);
   omp_set_num_threads(nthreads);
   unsigned long *distance_counters = calloc(nthreads * NDISTANCES, sizeof(*distance_counters));
+  coordinates_t *input_i = malloc(block_size * sizeof(*input_i));
+  coordinates_t *input_j = malloc(block_size * sizeof(*input_j));
+  coordinates_t * const input_j_backup = input_j;
+  char *buffer = malloc(block_size * 24);
 
 #pragma omp parallel
   {
     const int ithread = omp_get_thread_num();
-#pragma omp for schedule(dynamic, 100)
-    for (size_t i = 0; i < ncoordinates; i++)
-      for (size_t j = i+1; j < ncoordinates; j++)
-	distance_counters[NDISTANCES*ithread + compute_distance(input+i, input+j)]++;
 
+    for (size_t ib = 0; ib < ncoordinates; ib+=block_size) {
+      size_t ie = ib + block_size < ncoordinates ? block_size : ncoordinates -ib;
+      fseek(fp, ib, SEEK_SET);
+      fread(buffer, 1, 24 * block_size, fp);
+#pragma omp for
+      for (size_t m = 0; m < block_size; m++)
+	read_coordinates(input_i+m, buffer+24*m);
+
+      for (size_t jb = ib; jb < ncoordinates; jb+=block_size) {
+	size_t je = jb + block_size < ncoordinates ? block_size : ncoordinates - jb;
+	if (jb != ib) {
+	  fseek(fp, jb, SEEK_SET);
+	  fread(buffer, 1, 24 * block_size, fp);
+#pragma omp for
+	  for (size_t m = 0; m < block_size; m++)
+	    read_coordinates(input_j+m, buffer+24*m);
+	} else {
+	  input_j = input_i;
+	}
+#pragma omp for schedule(dynamic, 100)
+	for (size_t ix=0; ix < ie; ++ix)
+	  for (size_t jx = (ib < jb ? 0: ix+1); jx < je; ++jx)
+	    distance_counters[NDISTANCES*ithread + compute_distance(input_i+ix, input_j+jx)]++;
+	
+	input_j = input_j_backup;
+      }
+    }
 #pragma omp for
     for (size_t i = 0; i < NDISTANCES; i++)
       for (size_t j = 1; j < nthreads; j++)
 	distance_counters[i]+=distance_counters[NDISTANCES*j + i];
   }
-
-  free(input);
-  input = NULL;  
+  fclose(fp);
+  fp = NULL;
+  free(input_i);
+  free(input_j);
+  free(buffer);
+  input_i = input_j  = NULL;
+  buffer = NULL;
 
   for (size_t i = 0; i < NDISTANCES/100; i++) { //count up integer part of output
     for (size_t j = 0; j < 100; j++) { //count up rational part
